69a0abc282ca3fffbf866c5bea91f46604e1bd61
Mambafei Tanguy <mambafeitanguy@gmail.com>
2024-10-07 10:15:26 +0000

    Ajout de l'image à l'annonce

--- src/main/java/unchk/projects/lostnfound/controllers/AnnonceController.java
+++ src/main/java/unchk/projects/lostnfound/controllers/AnnonceController.java
@@ -1,12 +1,19 @@
 package unchk.projects.lostnfound.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import unchk.projects.lostnfound.implement.AnnonceServiceImplement;
 import unchk.projects.lostnfound.models.Annonces;
 import unchk.projects.lostnfound.repos.AnnonceRepository;
 import unchk.projects.lostnfound.services.AnnonceService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +21,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = {"/annonces"})
 public class AnnonceController {
-
+    @Autowired
     private final AnnonceRepository annonceRepository;
     private final AnnonceServiceImplement annonceService;
+    private static final String UPLOAD_DIR = "uploads/";
 
     public AnnonceController(AnnonceRepository annonceRepository, AnnonceServiceImplement annonceService) {
         this.annonceRepository = annonceRepository;
@@ -28,16 +36,32 @@ public class AnnonceController {
         return annonceRepository.findAll();
     }
 
-    @PostMapping
-    public Annonces addAnnonce(@RequestBody Annonces annonce) {
-        Date date = new Date();
-         annonce = new Annonces();
-         annonce.setTitre(annonce.getTitre());
-         annonce.setDescription(annonce.getDescription());
-         annonce.setLieu(annonce.getLieu());
-         annonce.setDate(annonce.getDate());
-         annonce.setDate_publication(date);
-        return annonceService.saveAnnonce(annonce);
+    @PostMapping( consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Annonces> addAnnonce(
+            @RequestPart("annonce") Annonces annonce,
+            @RequestPart("image") MultipartFile imageFile) throws IOException {
+
+        if (!imageFile.isEmpty()) {
+            String fileName = System.currentTimeMillis() + "_" + imageFile.getOriginalFilename();
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+            Files.createDirectories(path.getParent());
+            Files.write(path, imageFile.getBytes());
+            annonce.setUrl_image("/" + UPLOAD_DIR + fileName);
+
+        }
+
+        System.out.println("paqhsdhdhedh");
+
+        annonce.setTitre(annonce.getTitre());
+        annonce.setDescription(annonce.getDescription());
+        annonce.setLieu(annonce.getLieu());
+        annonce.setDate(new Date());
+        annonce.setStatus(true); // Par défaut, les annonces sont validées
+        annonce.setUser(annonce.getUser()); // Associer l'annonce à l'utilisateur courant
+
+        Annonces savedAnnonce = annonceService.saveAnnonce(annonce);
+
+        return ResponseEntity.ok(savedAnnonce);
     }
 
     @GetMapping("/{id}")
