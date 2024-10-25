251489159b8026e2db85a8419cc7801a05d271bd
malick <ndao894@gmail.com>
2024-10-25 18:49:52 +0000

    changes

--- src/main/java/unchk/projects/lostnfound/controllers/ChatController.java
+++ src/main/java/unchk/projects/lostnfound/controllers/ChatController.java
@@ -3,11 +3,13 @@ package unchk.projects.lostnfound.controllers;
 import java.util.List;  // Utiliser java.util.List
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import unchk.projects.lostnfound.models.Annonces;
 import unchk.projects.lostnfound.models.Message;
 import unchk.projects.lostnfound.services.MessageService;
 
@@ -20,7 +22,14 @@ public class ChatController {
 
     // Endpoint pour récupérer les messages par annonce
     @GetMapping("/annonce/{annonceId}")
-    public List<Message> getMessagesByAnnonce(@PathVariable Long annonceId) {
-        return messageService.findMessagesByAnnonceId(annonceId);
+    public ResponseEntity<List<Message>> getMessagesByAnnonce(@PathVariable Long annonceId) {
+        Annonces annonce = new Annonces();
+        annonce.setId(annonceId);
+        List<Message> messages = messageService.getMessagesByAnnonce(annonceId);
+        return ResponseEntity.ok(messages);
     }
+    
+    
+ 
+    
 }
